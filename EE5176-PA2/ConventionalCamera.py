fa9a30a3f2cf6d093e507feeea3c6711ba66c385
Sagnik Ghosh <ee19b132@smail.iitm.ac.in>
2022-03-12 19:46:05 +0530

    Moved functions to utils.py

--- EE5176-PA2/ConventionalCamera.py
+++ EE5176-PA2/ConventionalCamera.py
@@ -3,18 +3,7 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import scipy.io
 import scipy.linalg
-
-def zeropad(img, left_pad, right_pad):
-    
-    if left_pad:
-        img = np.concatenate((np.zeros((img.shape[0], left_pad, 3)), img), axis = 1)
-    if right_pad:
-        img = np.concatenate((img, np.zeros((img.shape[0], right_pad, 3))), axis = 1)
-
-    return img
-
-def get_args():
-    pass
+from utils import zeropad, deblur
 
 def main():
     ### Read Image ###
@@ -41,11 +30,7 @@ def main():
     plt.show()
 
     ### Deblurring - Least Squares ###
-    R_channel = scipy.linalg.lstsq(A, blurred_img[:,:,0].T)[0].T
-    G_channel = scipy.linalg.lstsq(A, blurred_img[:,:,1].T)[0].T
-    B_channel = scipy.linalg.lstsq(A, blurred_img[:,:,2].T)[0].T
-
-    deblurred = np.stack([R_channel, G_channel, B_channel], axis = 2)
+    deblurred = deblur(A, blurred_img)
     plt.imshow(deblurred)
     plt.show()
 
