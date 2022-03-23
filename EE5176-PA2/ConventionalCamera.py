8aef61c6ad836619242caa344c11896c985412c7
Sagnik Ghosh <ee19b132@smail.iitm.ac.in>
2022-03-23 10:42:23 +0530

    Modified saving code

--- EE5176-PA2/ConventionalCamera.py
+++ EE5176-PA2/ConventionalCamera.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import scipy.io
 import scipy.linalg
+import imageio
 from utils import zeropad, deblur, RMSE
 
 def main():
@@ -18,27 +19,25 @@ def main():
 
     noise = scipy.io.loadmat(r"data/gaussNoise")['gaussNoise']/255.0
     blurred_img+=noise
+    imageio.imwrite('results/CC_blurred.png', blurred_img*255.0)
     plt.imshow(blurred_img)
     plt.show()
-    save_img = Image.fromarray(blurred_img)
-    save_img.save("results/CC_blurred.png")
 
     ### Generate Toeplitz Matrix ###
     A = np.zeros((851, 800))
     for i in range(800):
         A[i:i+52,i] = 1/52.0
 
+    imageio.imwrite('results/CC_toeplitz.png', A*255.0)
     plt.imshow(A)
     plt.show()
-    save_img = Image.fromarray(A)
-    save_img.save("results/CC_toeplitz.png")
 
     ### Deblurring - Least Squares ###
     deblurred = deblur(A, blurred_img)
+
+    imageio.imwrite('results/CC_deblurred.png', deblurred*255.0)
     plt.imshow(deblurred)
     plt.show()
-    save_img = Image.fromarray(deblurred)
-    save_img.save("results/CC_deblurred.png")
 
     ### RMSE Value ###
     print(f"RMSE between Original And Deblurred = {RMSE(img, deblurred)}")
