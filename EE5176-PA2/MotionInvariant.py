ce09dfab9e8a5352b4600e5c34616e3b552ea1cf
Sagnik Ghosh <ee19b132@smail.iitm.ac.in>
2022-03-23 15:11:08 +0530

    Fixed Deblurring and saved figures

--- EE5176-PA2/MotionInvariant.py
+++ EE5176-PA2/MotionInvariant.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+import imageio
 import scipy.io
 
 from utils import create_frame, deblur
@@ -17,34 +18,46 @@ def main():
     for i in range(53):
         blurred_img1+=create_frame(img_fg, img_bg, i, 0)/53.0
 
+    imageio.imwrite('results/MI_blurred1.png', blurred_img1*255.0)
     plt.imshow(blurred_img1)
     plt.show()
 
     ### Generating Blurred Image (With Camera Motion) ###
     blurred_img2 = np.zeros((img_bg.shape[0], img_bg.shape[1]+53, 3))
-    bg_trans = np.rint(scipy.io.loadmat('data/CameraT.mat')['CameraT'].flatten()).astype(int)
+    bg_trans = np.floor(scipy.io.loadmat('data/CameraT.mat')['CameraT'].flatten()).astype(int)
     for i in range(53):
         blurred_img2+=create_frame(img_fg, img_bg, bg_trans[i]+i, bg_trans[i])/53.0
 
+    imageio.imwrite('results/MI_blurred2.png', blurred_img2*255.0)
     plt.imshow(blurred_img2)
     plt.show()
 
     ### Generating PSF ###
     PSF = np.zeros((53,))
     a=1/13
-    for i in bg_trans:
+    for i in range(53):
         if i:
             PSF[i]+=1/np.sqrt(a*i)
         else:
             PSF[i]+=1/np.sqrt(a*0.1)
     PSF = PSF/np.sum(PSF)
 
+    plt.stem(PSF)
+    plt.title(r"PSF of Motion Invariant Blur")
+    plt.savefig("results/MI_PSF.png")
+
+    ### Generating Blur Matrix ###
     A = np.zeros((653,600))
     for i in range(600):
         A[i:i+53, i] = PSF
 
+    imageio.imwrite('results/MI_toeplitz.png', A*255.0)
+    plt.imshow(A)
+    plt.show()
+
     deblurred = deblur(A, blurred_img2)
 
+    imageio.imwrite('results/MI_deblurred.png', deblurred*255.0)
     plt.imshow(deblurred)
     plt.show()
 
