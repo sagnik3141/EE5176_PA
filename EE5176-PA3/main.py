ae87085e3d25b8fa5e8603c9bc25f6c30709e87a
Ubuntu <ubuntu@ip-172-31-20-101.us-west-2.compute.internal>
2022-03-30 13:41:31 +0000

    AWS

--- EE5176-PA3/main.py
+++ EE5176-PA3/main.py
@@ -11,19 +11,24 @@ def main():
     Z = np.transpose(Z, (1,2,3,0))
     Z = Z.reshape((-1, 3, 16))
     ZR, ZG, ZB = [Z[:, i, :] for i in range(3)] # Separating Color Channels
+    print(ZR.shape)
     w = [z if z<=255/2 else 255-z for z in range(256)] # Weightage of each pixel value
     ZR = sampler(ZR, w)
-    ZG = sampler(ZR, w)
-    ZB = sampler(ZR, w)
+    print(ZR.shape)
+    ZG = sampler(ZG, w)
+    ZB = sampler(ZB, w)
+    print(ZB.shape)
 
     ### CRF Estimation ###
     l = 0.1 # Smoothness Constant
     B = [np.log((1/2048)*np.float_power(2, k)) for k in range(16)] # Log Exposures
 
     gR = estimateCRF(ZR, B, l, w)
-    gG = estimateCRF(ZR, B, l, w)
-    gB = estimateCRF(ZR, B, l, w)
+    gG = estimateCRF(ZG, B, l, w)
+    gB = estimateCRF(ZB, B, l, w)
     plt.plot(gR)
+    plt.plot(gG)
+    plt.plot(gB)
     plt.savefig('CRF.png')
 
 if __name__=="__main__":
--- EE5176-PA3/utils.py
+++ EE5176-PA3/utils.py
@@ -1,9 +1,10 @@
 import numpy as np
 
 def sampler(Z, w):
-    num_samples = Z.shape[0]*1e-6
+    num_samples = Z.shape[0]*1e-4
     #grid = [[(i, j) for j in range(16)] for i in range(Z.shape[0])]
-    p_dist = [w[Z[i][7]] for i in range(Z.shape[0])]
-    idx = np.random.choice(num_samples, num_samples, replace = False, p = p_dist)
+    p_dist = np.array([w[Z[i][7]] for i in range(Z.shape[0])])
+    p_dist = p_dist/np.sum(p_dist)
+    idx = np.random.choice(Z.shape[0], int(num_samples), replace = False, p = p_dist)
 
     return Z[idx]
