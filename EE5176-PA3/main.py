5cf697ec74c844b2e1a386b14f5b1d32c02d21a7
Ubuntu <ubuntu@ip-172-31-20-101.us-west-2.compute.internal>
2022-03-30 13:55:26 +0000

    Removed print statements

--- EE5176-PA3/main.py
+++ EE5176-PA3/main.py
@@ -11,13 +11,10 @@ def main():
     Z = np.transpose(Z, (1,2,3,0))
     Z = Z.reshape((-1, 3, 16))
     ZR, ZG, ZB = [Z[:, i, :] for i in range(3)] # Separating Color Channels
-    print(ZR.shape)
     w = [z if z<=255/2 else 255-z for z in range(256)] # Weightage of each pixel value
     ZR = sampler(ZR, w)
-    print(ZR.shape)
     ZG = sampler(ZG, w)
     ZB = sampler(ZB, w)
-    print(ZB.shape)
 
     ### CRF Estimation ###
     l = 0.1 # Smoothness Constant
@@ -26,10 +23,15 @@ def main():
     gR = estimateCRF(ZR, B, l, w)
     gG = estimateCRF(ZG, B, l, w)
     gB = estimateCRF(ZB, B, l, w)
-    plt.plot(gR)
-    plt.plot(gG)
-    plt.plot(gB)
-    plt.savefig('CRF.png')
+    plt.plot(gR, list(range(256)))
+    plt.plot(gG, list(range(256)))
+    plt.plot(gB, list(range(256)))
+    plt.legend(['CRF - R Channel', 'CRF - G Channel', 'CRF - B Channel'])
+    plt.xlabel('Log Exposure')
+    plt.ylabel('Pixel Values')
+    plt.title('CRF Plots')
+    plt.grid(True)
+    plt.savefig('results/CRF.png')
 
 if __name__=="__main__":
     main()
