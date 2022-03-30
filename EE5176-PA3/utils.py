3200e3183f78a4a7b54ca627e419bb46b9a251d5
Sagnik Ghosh <ee19b132@smail.iitm.ac.in>
2022-03-30 11:49:15 +0530

    Bug fix

--- EE5176-PA3/utils.py
+++ EE5176-PA3/utils.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 def sampler(Z, w):
-    num_samples = Z.shape[0]*Z.shape[1]*1e-6
-    grid = [[(i, j) for j in range(16)] for i in range(Z.shape[0])]
+    num_samples = Z.shape[0]*1e-6
+    #grid = [[(i, j) for j in range(16)] for i in range(Z.shape[0])]
     p_dist = [w[Z[i][7]] for i in range(Z.shape[0])]
-    idx = np.random.choice(grid, num_samples, replace = False, p = p_dist)
+    idx = np.random.choice(num_samples, num_samples, replace = False, p = p_dist)
 
     return Z[idx]
