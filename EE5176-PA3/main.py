cbdbc6be4146b85105a68860955303a8b02c17b6
Ubuntu <ubuntu@ip-172-31-20-101.us-west-2.compute.internal>
2022-03-30 17:00:37 +0000

    Added code to get HDR Image

--- EE5176-PA3/main.py
+++ EE5176-PA3/main.py
@@ -4,25 +4,27 @@ from PIL import Image
 
 from CRF import estimateCRF
 from utils import sampler
+from getHDR import getHDRColor
 
 def main():
     ### Load Images ###
     Z = np.array([np.array(Image.open(f"Data/exposure{i}.jpg")) for i in range(1,17)])
+    _, X_dim, Y_dim, __ = Z.shape
     Z = np.transpose(Z, (1,2,3,0))
     Z = Z.reshape((-1, 3, 16))
     ZR, ZG, ZB = [Z[:, i, :] for i in range(3)] # Separating Color Channels
-    w = [z if z<=255/2 else 255-z for z in range(256)] # Weightage of each pixel value
-    ZR = sampler(ZR, w)
-    ZG = sampler(ZG, w)
-    ZB = sampler(ZB, w)
+    w = np.array([z if z<=255/2 else 255-z for z in range(256)]) # Weightage of each pixel value
+    ZR_ = sampler(ZR, w)
+    ZG_ = sampler(ZG, w)
+    ZB_ = sampler(ZB, w)
 
     ### CRF Estimation ###
     l = 0.1 # Smoothness Constant
     B = [np.log((1/2048)*np.float_power(2, k)) for k in range(16)] # Log Exposures
 
-    gR = estimateCRF(ZR, B, l, w)
-    gG = estimateCRF(ZG, B, l, w)
-    gB = estimateCRF(ZB, B, l, w)
+    gR = estimateCRF(ZR_, B, l, w)
+    gG = estimateCRF(ZG_, B, l, w)
+    gB = estimateCRF(ZB_, B, l, w)
     plt.plot(gR, list(range(256)))
     plt.plot(gG, list(range(256)))
     plt.plot(gB, list(range(256)))
@@ -32,6 +34,11 @@ def main():
     plt.title('CRF Plots')
     plt.grid(True)
     plt.savefig('results/CRF.png')
+    
+    ### HDR Radiance Map ###
+    RGB_HDR = getHDRColor(ZR, ZG, ZB, gR, gG, gB, w, B, X_dim, Y_dim)
+    
+    
 
 if __name__=="__main__":
     main()
